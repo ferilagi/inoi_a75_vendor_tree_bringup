f35c610f2626644462a30ef13899f01e4e82ea90
ferilagi <ferilagi@example.com>
2026-09-22 22:05:10 +0700

    prune

--- A75-vendor.mk
+++ A75-vendor.mk
@@ -729,7 +729,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@6.0 \
     android.hardware.bluetooth.audio@2.1 \
     android.hardware.bluetooth@1.0 \
-    android.hardware.bluetooth@1.1 \
     android.hardware.boot@1.2 \
     android.hardware.drm@1.0 \
     android.hardware.drm@1.1 \
